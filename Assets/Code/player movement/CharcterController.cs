bd8674501c64e470a6712bf594b7054c70620be1
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-03-16 01:17:20 +0000

    found camera move error
    wont move along x axis cos code is for 3rd person camera

--- Assets/Code/player movement/CharcterController.cs
+++ Assets/Code/player movement/CharcterController.cs
@@ -40,7 +40,8 @@ public class CharcterController : MonoBehaviour
         rb.velocity = new Vector3(newVelocity.x, rb.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));       
+        // WONT WORK COS CODE IS FOR 3RD PERSON CAMERA NOT 1ST PERSON
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
