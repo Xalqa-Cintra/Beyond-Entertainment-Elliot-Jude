f1aed3bf91bf2646d6f84491d1acec60d3e289a1
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-03-20 23:50:08 +0000

    fixed error

--- Assets/Code/BillBoarding/SpriteBillBoard.cs
+++ Assets/Code/BillBoarding/SpriteBillBoard.cs
@@ -4,6 +4,6 @@ public class SpriteBillBoard : MonoBehaviour
 { 
     private void Update()
     {
-        transform.position = Quaternion.Euler(0f,Camera.main.transform.rotation.eulerAngles.y,0f);
+        transform.rotation = Quaternion.Euler(0f,Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
