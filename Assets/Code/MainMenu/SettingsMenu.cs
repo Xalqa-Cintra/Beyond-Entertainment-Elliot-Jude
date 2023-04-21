14ef58dc95d7d4129fb4069d50fe3301aeed7faa
elliotmarsden <113114324+elliotmarsden@users.noreply.github.com>
2023-04-21 12:41:41 +0100

    small chnages

--- Assets/Code/MainMenu/SettingsMenu.cs
+++ Assets/Code/MainMenu/SettingsMenu.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer mainMixer;
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("Volume", volume);
+    }
  
     public void SetFullscreen(bool isFullscreen)
     {
