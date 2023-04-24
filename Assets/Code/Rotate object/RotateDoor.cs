084a64e47172557c749a99ee91c45c22824ebd39
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-04-24 11:47:44 +0100

    door buggy

--- Assets/Code/Rotate object/RotateDoor.cs
+++ Assets/Code/Rotate object/RotateDoor.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class RotateDoor : MonoBehaviour
 {
+    public GameObject darkroomManager;
+    public Transform rotatePoint;
+
+    public float rotateTime;
+    public float rotateAmount;
+
+    private void Update()
+    {
+        if (rotateTime > 0)
+        {
+            if (darkroomManager.GetComponent<DarkRoomPhotos>().currentPhoto > 2)
+            {
+                DoorRotate();
+            }
+        }
+    }
+
+    private void DoorRotate()
+    {
+        transform.RotateAround(rotatePoint.position, Vector3.up, rotateAmount);
+        rotateTime =- Time.deltaTime;
+    }
 
 }
