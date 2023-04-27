fe2bd0429057ff066c182ba8767efb22dc96c38a
Alex-Loves-Big-Chungus <126978772+Alex-Loves-Big-Chungus@users.noreply.github.com>
2023-04-27 14:53:44 +0100

    credits menu and biomes code thiking

---
+++ Assets/Code/BiomesNames.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiomesNames : MonoBehaviour
+{
+  // try to find a grid way to select the grid for a bit of map
+  // if player is in box 1 then name "no mans land" or gameobject
+  // if player is in box 2 then name "red Room" or gameobject
+  // if player is in box 3 then name "Bunker" or gameobject
+  // else the player is name "wilderness" 
+  // to set the areas as triggers to see if the player is on it
+  // try to link a mini map up with this biomes names
+}
