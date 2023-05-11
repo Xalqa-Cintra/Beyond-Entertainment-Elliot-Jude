d1b4f9f7d8869599d08cdaa8717daf39c3edad40
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-05-11 15:13:31 +0100

    end game works woo

--- Assets/Code/LoadingNextScene.cs
+++ Assets/Code/LoadingNextScene.cs
@@ -5,22 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class LoadingNextScene : MonoBehaviour
 {
-    float countdown;
     // Start is called before the first frame update
     void Start()
     {
-        countdown = 9;
-        countdown = -Time.deltaTime;
-        if (countdown <= 0)
-        {
             SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
